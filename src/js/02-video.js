3c8f5ed4a3a9fec354c0a686d017da74760cdfdb
ghhjhkjkklkl <99083436+ghhjhkjkklkl@users.noreply.github.com>
2022-09-03 16:16:05 +0300

    start#4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+ import Player from '@vimeo/player';
 
+
+
+ const iframeEl = document.querySelector('#vimeo-player');
+ const player = new Player(iframeEl);
+
+
+const newTime = player.on('timeupdate', function(data) {
+});
+
+localStorage.setItem("videoplayer-current-time", "newTime");
+
+player.setCurrentTime(30.456).then(function (seconds) {
+         // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+         case 'RangeError':
+             // the time was less than 0 or greater than the video’s duration
+           break;
+
+         default:
+            // some other error occurred
+             break;
+     }
+ });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle'
+
+const STOREGE_KEY = 'feedback-form-state';
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    email: document.querySelector('.feedback-form input'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+    
+
+populateTextarea();
+ 
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STOREGE_KEY);
+};
+        
+
+ function onTextareaInput(evt) {
+     const message = evt.target.value;
+     localStorage.setItem(STOREGE_KEY, message);
+    
+     };
+
+function populateTextarea() {
+    const savedformDate = localStorage.getItem(STOREGE_KEY);
+    if (savedformDate) {
+        JSON.parse(savedformDate);
+        formDate[evt.target.name] = evt.target.value;
+    }
+} 
+
+const formDate = {};
+refs.form.addEventListener('input', (evt) => {--__
+    localStorage.setItem(STOREGE_KEY, JSON.stringify(formDate));
+});
+
+
 
