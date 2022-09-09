8a98b380fcbcd393ee91e76564a96b06c5d53f26
ghhjhkjkklkl <99083436+ghhjhkjkklkl@users.noreply.github.com>
2022-09-09 19:05:25 +0300

    start#9

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,15 +5,15 @@ import throttle from 'lodash.throttle';
  const iframeEl = document.querySelector('#vimeo-player');
  const player = new Player(iframeEl);
 
- player.on('timeupdate', function (data) {
+ player.on('timeupdate', throttle(function (data) {
   localStorage.setItem("videoplayer-current-time", data.seconds);
   console.log(data.seconds);
-});
+}), 500);
 
 
-let savedTime = localStorage.getItem("videoplayer-current-time", data.seconds);
+let savedTime = localStorage.getItem("videoplayer-current-time");
+console.log(savedTime);
  
   if (savedTime) {
     player.setCurrentTime(savedTime);
-    savedTime.value = player;
-  }
+};
